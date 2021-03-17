2619f7347b0474ea75657ebb65c641d6b8f4d772
Alessandro Marega <ale_marega@hotmail.it>
2021-03-17 15:53:48 +0100

    aggiunto array di oggetti 'studenti'

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
@@ -18,3 +17,27 @@ var studente = {
 for (var key in studente) {
   console.log(key + ": " + studente[key]);
 }
+
+// 3. creo un array di oggetti 'studenti'
+var studenti = [
+  {
+    nome: Alessandro,
+    cognome: Marega,
+    eta: 28
+  },
+  {
+    nome: Mario,
+    cognome: Rossi,
+    eta: 30
+  },
+  {
+    nome: Tazio,
+    cognome: Nuvolari,
+    eta: 65
+  },
+  {
+    nome: Pippo,
+    cognome: Sempronio,
+    eta: 53
+  },
+];
