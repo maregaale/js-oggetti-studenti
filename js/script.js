38b7b5bced60369dd910a72ca4ab0ca3bf4ddcaa
Alessandro Marega <ale_marega@hotmail.it>
2021-03-17 15:44:17 +0100

    aggiunto oggetto che descrive lo studente

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+// variabili
+
+
+// funzioni
+
+// 1. creo un oggetto che descriva uno studente
+var studente = {
+  nome: "Alessandro",
+  cognome: "Marega",
+  eta: "28"
+};
