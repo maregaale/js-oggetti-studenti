d76a8c73351f779268dabbc1beb4d4ae40f927d8
Alessandro Marega <ale_marega@hotmail.it>
2021-03-17 15:48:49 +0100

    aggiunta stampa in console delle proprietà dell'oggetto 'studente'

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 // Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
@@ -14,3 +13,8 @@ var studente = {
   cognome: "Marega",
   eta: "28"
 };
+
+// 2. stampo in console le proprietà dell'oggetto 'studente'
+for (var key in studente) {
+  console.log(key + ": " + studente[key]);
+}
