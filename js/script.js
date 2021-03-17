1328495d31b586c7fb5fb659dd45476bf457a5ed
Alessandro Marega <ale_marega@hotmail.it>
2021-03-17 16:46:40 +0100

    aggiunto oggetto con prompt utente e inserito nell'array studenti, stampa in concole

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-
-// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-
-// variabili
-
-
-// funzioni
-
 // 1. creo un oggetto che descriva uno studente
 var studente = {
   nome: "Alessandro",
@@ -46,3 +38,36 @@ var studenti = [
 for (var i = 0; i < studenti.length; i++) {
   console.log(studenti[i]["nome"] + " " + studenti[i]["cognome"]);
 }
+
+//5. Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+// creo un oggetto vuoto
+var nuovoStudente = {};
+
+// chiedo nome, cognome, età all'utente
+var nomeUtente = prompt("inserisci il nome dello studente");
+var cognomeUtente = prompt("inserisci il cognome dello studente");
+var etaUtente = parseInt(prompt("inserisci l'età dello studente"));
+
+// trasformo la prima lettera delle variabili inserite dall'utente in maiuscola
+nomeUtente = nomeUtente.charAt(0).toUpperCase() + nomeUtente.slice(1);
+cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
+
+// controllo che l'utente compili tutti i campi, che l'etaUtente sia un numero, quindi aggiungo le proprietà nome, cognome e età all'oggetto vuoto
+if (nomeUtente == "" || cognomeUtente == "" || etaUtente == "") {
+  alert("compila tutti i campi di 'nome', 'cognome' e 'età'");
+} else {
+  if (isNaN(etaUtente) == false) {
+    nuovoStudente["nome"] = nomeUtente;
+    nuovoStudente["cognome"] = cognomeUtente;
+    nuovoStudente["eta"] = etaUtente;
+  } else {
+    alert("inserisci un numero per l'età");
+  }
+}
+
+// aggiungo il nuovo oggetto all'array di studenti
+studenti.push(nuovoStudente);
+
+// stampo l'array con il nuovo oggetto
+console.log(studenti);
